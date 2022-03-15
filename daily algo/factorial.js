9b7bd337459ddfa7203cbd59c7394e6cc6de1f49
Katherine Iannaccone <katherineiannaccone@yahoo.com>
2022-03-15 17:14:35 -0600

    staircase

---
+++ daily algo/factorial.js
@@ -0,0 +1,60 @@
+// The factorial of the integer , written , is defined as:
+
+// Calculate and print the factorial of a given integer.
+
+// For example, if , we calculate  and get .
+
+// Function Description
+
+// Complete the extraLongFactorials function in the editor below. It should print the result and return.
+
+// extraLongFactorials has the following parameter(s):
+
+// n: an integer
+// Note: Factorials of  can't be stored even in a  long long variable. Big integers must be used for such calculations. Languages like Java, Python, Ruby etc. can handle big integers, but we need to write additional code in C/C++ to handle huge values.
+
+// We recommend solving this challenge using BigIntegers.
+
+// Input Format
+
+// Input consists of a single integer 
+
+// Constraints
+
+
+// Output Format
+
+// Print the factorial of .
+
+// Sample Input
+
+
+// Sample Output
+
+
+
+
+
+function extraLongFactorials(n) {
+
+    n = BigInt(n)
+    var accumulator = BigInt(1)
+
+    for (let i = n; i > 0; i--) {
+        accumulator = accumulator * i
+    }
+
+    console.log(accumulator.toString())
+}
+
+function extraLongFactorials(n) {
+    let bigInt = BigInt(n);
+    let factorial = 1n;
+
+    for (let i = 0n; i < bigInt ; i++) {
+        factorial *= bigInt - i;
+    }
+    
+    console.log(factorial.toString());
+    
+}
